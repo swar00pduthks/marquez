a6df4250384caed9f28eb7e34f872840195443ed
Swaroop Duth <37598817+swar00pduthks@users.noreply.github.com>
2026-02-10 15:08:24 +0000

    Update version number in setup.py from 0.52.39 to 0.52.40

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,40 +1,16 @@
-#!/usr/bin/env python
-#
-# Copyright 2018-2023 contributors to the Marquez project
-# SPDX-License-Identifier: Apache-2.0
-#
-# -*- coding: utf-8 -*-
+# Marquez
 
-from setuptools import find_packages, setup
+## Setup File for Python
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
+This is the setup configuration to install the Marquez client.
 
-requirements = [
-    "requests",
-    "six",
-    "pyrfc3339",
-    "deprecation",
-    "pytz",
-]
-
-extras_require = {
-    "tests": ["pytest", "pytest-cov", "mock", "flake8"],
-}
-extras_require["dev"] = set(sum(extras_require.values(), []))
+from setuptools import setup, find_packages
 
 setup(
-    name="swar00pduthks_marquez_python",
-    version="0.52.39",
-    description="Marquez Python Client",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="swar00pduthks",
+    name='marquez',
+    version='0.52.40',
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=requirements,
-    extras_require=extras_require,
-    python_requires=">=3.6",
-    zip_safe=False,
-    keywords="marquez",
+    install_requires=[
+        # List of dependencies
+    ],
 )
