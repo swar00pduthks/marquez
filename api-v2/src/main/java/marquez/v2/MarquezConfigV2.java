9edd9e22d14eddbccd86046f5e5e380099a48bc8
swar00pduthks <37598817+swar00pduthks@users.noreply.github.com>
2026-03-13 23:33:55 +0000

    chore: add missing apache copyright headers to api-v2
    - Added the standard Apache 2.0 copyright header to `MarquezAppV2.java`, `MarquezConfigV2.java`, and `LineageResourceV2.java` to fix GitHub Actions CI `Check for Headers` failure.

--- api-v2/src/main/java/marquez/v2/MarquezConfigV2.java
+++ api-v2/src/main/java/marquez/v2/MarquezConfigV2.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018-2023 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package marquez.v2;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
